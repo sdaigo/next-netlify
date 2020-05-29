a6bd37fa25bea87dd4a3aac0d9ae37a7f7d5b2cd
Daigo Shitara <stain.witness+github@gmail.com>
2020-05-29 14:58:39 +0900

    fix path

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,27 @@
 import * as React from 'react'
 import Head from 'next/head'
 
+import { attributes, react as HomeContent } from '../../content/home.md'
+
 export default () => {
+  let { title, cats } = attributes
   return (
     <>
       <Head>
         <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
       </Head>
-      <article>hello</article>
+      <article>
+        <h1>{title}</h1>
+        <HomeContent />
+        <ul>
+          {cats.map((cat, k) => (
+            <li key={k}>
+              <h2>{cat.name}</h2>
+              <p>{cat.description}</p>
+            </li>
+          ))}
+        </ul>
+      </article>
     </>
   )
 }
