82e3a1716a6940b6ad2d940a8fcd64c18deb8c20
Daigo Shitara <stain.witness+github@gmail.com>
2020-05-29 14:09:57 +0900

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import Head from 'next/head'
+
+export default () => {
+  return (
+    <>
+      <Head>
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+      </Head>
+      <article>hello</article>
+    </>
+  )
+}
